0dcdd193dfdd5cca406605e2d5df4437fe7c46bc
ushiboy <ushiboy.dev@gmail.com>
2020-05-07 22:01:55 +0900

    fix ATCommunicator test

--- src/ATCommunicator.spec.ts
+++ src/ATCommunicator.spec.ts
@@ -3,10 +3,10 @@ import { ATCommunicator } from './ATCommunicator';
 import { Connection, Unsubscribe } from './Connection';
 
 type MockConnectionState = {
-  opened: boolean,
+  opened?: boolean,
   openError?: Error,
-  sendBuffers: Buffer[],
-  receiveBuffers: Buffer[]
+  sendBuffers?: Buffer[],
+  receiveBuffers?: Buffer[]
 };
 
 class MockConnection implements Connection {
@@ -16,6 +16,15 @@ class MockConnection implements Connection {
   private subscriber: (buf: Buffer) => void
 
   constructor(state: MockConnectionState) {
+    if (state.opened == null) {
+      state.opened = false;
+    }
+    if (state.sendBuffers == null) {
+      state.sendBuffers = [];
+    }
+    if (state.receiveBuffers == null) {
+      state.receiveBuffers = [];
+    }
     this.state = state;
     this.subscriber = (buf: Buffer) => {};
   }
@@ -62,141 +71,124 @@ class MockConnection implements Connection {
 
 describe('ATCommunicator', () => {
   describe('connect', () => {
-    it('should open connection', async () => {
+    it('should open the connection', async () => {
       const at = new ATCommunicator(new MockConnection({
-        opened: false,
-        sendBuffers: [],
-        receiveBuffers: []
+        opened: false
       }));
-
       await assert.doesNotReject(async () => {
         await at.connect();
       });
     });
-    context('case of already open', () => {
-      context('if the isOpen property is true', () => {
-        it('should not be an error', async () => {
-          const at = new ATCommunicator(new MockConnection({
-            opened: true,
-            sendBuffers: [],
-            receiveBuffers: []
-          }));
-          await assert.doesNotReject(async () => {
-            await at.connect();
-          });
-        });
+    it('should not be an error if the isOpen property is true', async () => {
+      const at = new ATCommunicator(new MockConnection({
+        opened: true
+      }));
+      await assert.doesNotReject(async () => {
+        await at.connect();
       });
-      context('when a "Port opening error" error occurs', () => {
-        it('should not be an error', async () => {
-          const at = new ATCommunicator(new MockConnection({
-            opened: false,
-            openError: Error('Port is opening'),
-            sendBuffers: [],
-            receiveBuffers: []
-          }));
-          await assert.doesNotReject(async () => {
-            await at.connect();
-          });
-        });
+    });
+    it('should not be an error if you get a "Port opening error"', async () => {
+      const at = new ATCommunicator(new MockConnection({
+        opened: false,
+        openError: Error('Port is opening')
+      }));
+      await assert.doesNotReject(async () => {
+        await at.connect();
       });
-    })
-    context('case of other errors', () => {
-      it('should be an error', async () => {
-        const at = new ATCommunicator(new MockConnection({
-          opened: false,
-          openError: Error('Other'),
-          sendBuffers: [],
-          receiveBuffers: []
-        }));
-        await assert.rejects(async () => {
-          await at.connect();
-        }, {
-          name: 'Error',
-          message: 'Other'
-        });
+    });
+    it('should be an error if an unintended error occurs', async () => {
+      const at = new ATCommunicator(new MockConnection({
+        opened: false,
+        openError: Error('Other')
+      }));
+      await assert.rejects(async () => {
+        await at.connect();
+      }, {
+        name: 'Error',
+        message: 'Other'
       });
     });
   });
-  describe('write', () => {
-    it('should write a buffer to the connection', async () => {
+  describe('send', () => {
+    it('should write a buffer of AT commands', async () => {
       const state = {
-        opened: true,
-        sendBuffers: [],
-        receiveBuffers: []
+        sendBuffers: []
       };
       const at = new ATCommunicator(new MockConnection(state));
       await at.send(0x01);
       assert(state.sendBuffers[0]
         .equals(Buffer.from('AT*PIC=\x01\x00\x00\x01\r\n')));
     });
+    it('should write a buffer of AT commands, including data', async () => {
+      const state = {
+        sendBuffers: []
+      };
+      const at = new ATCommunicator(new MockConnection(state));
+      await at.send(0x01, Buffer.from([0x02]));
+      assert(state.sendBuffers[0]
+        .equals(Buffer.from('AT*PIC=\x01\x01\x00\x02\x02\r\n')));
+    });
   });
   describe('receive', () => {
-    it('should receive command buffer from connection', async () => {
+    it('should receive a buffer and return a command response', async () => {
       const at = new ATCommunicator(new MockConnection({
-        opened: true,
-        sendBuffers: [],
         receiveBuffers: [
-          Buffer.from('*PIC:\x01\x00\x00\x01\r\nOK\r\n'),
-          Buffer.from('*PIC:\x02\x00\x00\x02\r\nERROR\r\n'),
-          Buffer.from('*PIC:\x01\x00\x00\x01'),
-          Buffer.from('\r\nOK\r\n'),
-          Buffer.from('*PIC:\x02\x00\x00\x02'),
-          Buffer.from('\r\nERROR\r\n'),
           Buffer.from('*PIC:\x01\x00\x00\x01\r\n'),
-          Buffer.from('OK\r\n'),
-          Buffer.from('*PIC:\x03\x00\x00\x03\r\n'),
-          Buffer.from('ERROR\r\n')
+          Buffer.from('OK\r\n')
         ]
       }));
       const r1 = await at.receive();
       assert(r1.status === 0x01);
       assert(r1.size === 0x00);
       assert(r1.parity === 0x01);
-      const r2 = await at.receive();
-      assert(r2.status === 0x02);
-      assert(r2.size === 0x00);
-      assert(r2.parity === 0x02);
-      const r3 = await at.receive();
-      assert(r3.status === 0x01);
-      assert(r3.size === 0x00);
-      assert(r3.parity === 0x01);
-      const r4 = await at.receive();
-      assert(r4.status === 0x02);
-      assert(r4.size === 0x00);
-      assert(r4.parity === 0x02);
-      const r5 = await at.receive();
-      assert(r5.status === 0x01);
-      assert(r5.size === 0x00);
-      assert(r5.parity === 0x01);
-      const r6 = await at.receive();
-      assert(r6.status === 0x03);
-      assert(r6.size === 0x00);
-      assert(r6.parity === 0x03);
     });
-    context('timeout case', () => {
-      it('should get a timeout error', async () => {
-        const at = new ATCommunicator(new MockConnection({
-          opened: true,
-          sendBuffers: [],
-          receiveBuffers: [
-            Buffer.from('*PIC:01000001\r\nOK\r')
-          ]
-        }));
-        await assert.rejects(async () => {
-          await at.receive(10);
-        }, {
-            name: 'Error',
-            message: 'Timeout'
-        });
+    it('should be an error if the buffer times out during reception', async () => {
+      const at = new ATCommunicator(new MockConnection({
+        receiveBuffers: [
+          Buffer.from('*PIC:\x01\x00\x00\x01\r\n'),
+          Buffer.from('OK\r')
+        ]
+      }));
+      await assert.rejects(async () => {
+        await at.receive(10);
+      }, {
+        name: 'Error',
+        message: 'Timeout'
+      });
+    });
+    it('should be an error if the format of the received buffer is incorrect', async () => {
+      const at = new ATCommunicator(new MockConnection({
+        receiveBuffers: [
+          Buffer.from('*PIC:\x01\x00\x00\x01\r\n'),
+          Buffer.from('OK\r0')
+        ]
+      }));
+      await assert.rejects(async () => {
+        await at.receive();
+      }, {
+        name: 'Error',
+        message: 'Invalid data'
+      });
+    });
+    it('should be an error if the parity is incorrect', async () => {
+      const at = new ATCommunicator(new MockConnection({
+        receiveBuffers: [
+          Buffer.from('*PIC:\x01\x00\x00\x02\r\n'),
+          Buffer.from('OK\r\n')
+        ]
+      }));
+      await assert.rejects(async () => {
+        await at.receive();
+      }, {
+        name: 'Error',
+        message: 'Invalid parity'
       });
     });
   });
   describe('disconnect', () => {
-    it('should close connection', async () => {
+    it('should close the connection', async () => {
         const state = {
-          opened: true,
-          sendBuffers: [],
-          receiveBuffers: []
+          opened: true
         };
         const at = new ATCommunicator(new MockConnection(state));
         await at.disconnect();
